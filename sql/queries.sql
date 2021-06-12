d8e6e2badd29f5e7c2f40b90db91797963d07d42
Renzo <rmolivares@renzo-olivares.dev>
2021-06-11 22:01:47 -0700

    Add query for 7

--- sql/queries.sql
+++ sql/queries.sql
@@ -21,3 +21,40 @@ FROM Doctor D, (SELECT dept_ID FROM Department
 WHERE name = Allergy and Immunology) AS temp 
 WHERE temp.dept_ID = D.did) AS temp2) AS temp3 
 WHERE temp3.appt_id = A.appnt_ID AND A.status = 'AV' AND A.adate = 01/13/2020
+
+
+SELECT PA.doctor_id, PA.count AS Past, AC.count AS Active, AV.count AS Available, WL.count AS Waitlisted
+FROM (SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'PA' GROUP BY H.doctor_id) AS PA,
+(SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'AC' GROUP BY H.doctor_id) AS AC,
+(SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'AV' GROUP BY H.doctor_id) AS AV,
+(SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'WL' GROUP BY H.doctor_id) AS WL
+GROUP BY PA.doctor_id, pa.count, ac.count, av.count, wl.count;
+ORDER BY COUNT()
+
+
+SELECT H.doctor_id, COUNT(CASE WHEN A.status = 'PA' THEN 1 ELSE 0 END) AS Past, COUNT(CASE WHEN A.status = 'AC' THEN 1 ELSE 0 END) AS Active, COUNT(CASE WHEN A.status = 'AV' THEN 1 ELSE 0 END) AS Available, COUNT(CASE WHEN A.status = 'WL' THEN 1 ELSE 0 END) AS Waitlisted
+FROM has_appointment H, Appointment A
+WHERE H.appt_id = A.appnt_ID
+GROUP BY H.doctor_id;
+
+
+SELECT H.doctor_id, COUNT(A.status = 'PA') AS Past
+FROM has_appointment H, Appointment A 
+WHERE H.appt_id = A.appnt_ID
+GROUP BY H.doctor_id, A.status;
+
+
+
+
+
+
+SELECT DISTINCT * FROM Appointment A, (SELECT H.doctor_id, H.appt_id FROM has_appointment H, (SELECT D.doctor_ID FROM Doctor D, (SELECT dept_ID FROM Department WHERE name = 'Allergy and Immunology') AS temp WHERE temp.dept_ID = D.did) AS temp2) AS temp3 WHERE temp3.appt_id = A.appnt_ID AND A.status = 'AV';
+
+
+SELECT PA.doctor_id, COALESCE(PA.count,0) AS Past, COALESCE(AC.count,0) AS active, COALESCE(AV.count,0) AS Available, COALESCE(WL.count,0) AS Waitlisted
+FROM (SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'PA' GROUP BY H.doctor_id) AS PA LEFT JOIN 
+(SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'AC' GROUP BY H.doctor_id) AS AC ON PA.doctor_id = AC.doctor_id LEFT JOIN
+(SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'AV' GROUP BY H.doctor_id) AS AV ON AC.doctor_id = AV.doctor_id LEFT JOIN
+(SELECT H.doctor_id, COUNT(*) FROM has_appointment H, Appointment A WHERE H.appt_id = A.appnt_ID AND A.status = 'WL' GROUP BY H.doctor_id) AS WL ON AV.doctor_id = WL.doctor_id
+GROUP BY PA.doctor_id, pa.count, ac.count, av.count, wl.count
+ORDER BY PA.doctor_id DESC, pa.count DESC, ac.count DESC, av.count DESC, wl.count DESC;
